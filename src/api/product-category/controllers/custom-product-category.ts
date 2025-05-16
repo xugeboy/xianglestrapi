0bce3f60f4407c4fd6315ea9cf03bc7dcc2e9981
xugeboy <xugeboy@gmail.com>
2025-05-16 11:24:59 +0800

    modified:   src/api/product-category/controllers/custom-product-category.ts

--- src/api/product-category/controllers/custom-product-category.ts
+++ src/api/product-category/controllers/custom-product-category.ts
@@ -203,6 +203,7 @@ export default factories.createCoreController(
       try {
         const { slug: inputSlug } = ctx.params as { slug: string }; 
         const locale = ctx.query.locale;
+        const previousLocale = ctx.query.previousLocale;
         
         if (!inputSlug) {
           return ctx.badRequest("Input slug is required");
@@ -216,6 +217,7 @@ export default factories.createCoreController(
               fields: ['id', 'locale', 'slug'] 
             } 
           },
+          locale: previousLocale
         });
   
         if (!productsWithAnySlug || productsWithAnySlug.length === 0) {
